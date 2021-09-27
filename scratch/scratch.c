2954fbcc7e84c21c1e9251b11e8544e6ff1b1e32
Toby Hutton <toby@grusly.com>
2021-09-27 20:20:49 +1000

    Use `grit` to convert a PNG to tiles and load them into a bg.

--- scratch/scratch.c
+++ scratch/scratch.c
@@ -1,5 +1,6 @@
 #include <gba.h>
 
+#include "white-all.h"
 
 void init_gfx() {
   // Set the video mode to 0, enable bg 0, enable objects.
@@ -16,40 +17,14 @@ void init_gfx() {
   REG_BG0HOFS = 0;
   REG_BG0VOFS = 0;
 
-  // (1) Create 2 4bpp tiles: 1x basic tile and 1x cross
-  const u32 tiles[2][8] = {
-    { 0x11111111,
-      0x01111111,
-      0x01111111,
-      0x01111111,
-      0x01111111,
-      0x01111111,
-      0x01111111,
-      0x00000001 },
+  CpuFastSet(white_allTiles, CHAR_BASE_ADR(0), 512 | COPY32);
 
-    { 0x00000000,
-      0x00100100,
-      0x01100110,
-      0x00011000,
-      0x00011000,
-      0x01100110,
-      0x00100100,
-      0x00000000 },
-  };
-  CpuFastSet(tiles[0], CHAR_BASE_ADR(0), 16 | COPY16);
-  CpuFastSet(tiles[1], CHAR_BASE_ADR(1), 16 | COPY16);
+  CpuFastSet(white_allPal, BG_PALETTE, 16 | COPY16);
 
-  // (2) Create 4 separate palettes, each with empty index 0 and a different colour at index 1.
-  BG_PALETTE[0 * 16 + 1] = RGB5(31, 0, 0);     // Red
-  BG_PALETTE[1 * 16 + 1] = RGB5(0, 31, 0);     // Green
-  BG_PALETTE[2 * 16 + 1] = RGB5(0, 0, 31);     // Blue
-  BG_PALETTE[3 * 16 + 1] = RGB5(16, 16, 16);   // Grey
-
-  // (3) Create a map, 4 blocks of tiles, each using a different palette.
   u16* screen_block = SCREEN_BASE_BLOCK(SCR_BASE_BLOCK);
   for (int block_idx = 0; block_idx < 4; block_idx++) {
     for (int tile_idx = 0; tile_idx < 1024; tile_idx++) {
-      screen_block[block_idx * 1024 + tile_idx] = CHAR_PALETTE(block_idx) | 0;
+      screen_block[block_idx * 1024 + tile_idx] = (u16)(tile_idx & 0x3f);
     }
   }
 }
---
+++ scratch/white-all.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(white_all)
+
+//======================================================================
+//
+//	white_all, 64x64@4, 
+//	+ palette 256 entries, not compressed
+//	+ 64 tiles not compressed
+//	Total size: 512 + 2048 = 2560
+//
+//	Time-stamp: 2021-09-27, 20:02:36
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_WHITE_ALL_H
+#define GRIT_WHITE_ALL_H
+
+#define white_allTilesLen 2048
+extern const unsigned int white_allTiles[512];
+
+#define white_allPalLen 512
+extern const unsigned short white_allPal[256];
+
+#endif // GRIT_WHITE_ALL_H
+
+//}}BLOCK(white_all)
