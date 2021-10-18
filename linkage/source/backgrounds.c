d8b8861c790664ff2794e26bf3b22f1537e362c9
Toby Hutton <toby@grusly.com>
2021-10-18 23:00:00 +1100

    Update the puzzle tile graphics.
    It's now a nice glowing magenta neon looking thing.  The palette is only
    16 colours and set up to allow palette tricks to dullen or light up a
    puzzle piece.

--- linkage/source/backgrounds.c
+++ linkage/source/backgrounds.c
@@ -1,7 +1,7 @@
 #include <gba.h>
 
 #include "backgrounds.h"
-#include "white-all.h"
+#include "pieces.h"
 
 // -------------------------------------------------------------------------------------------------
 
@@ -10,9 +10,8 @@ void init_puzzle_bg() {
   BGCTRL[PUZZLE_BG_IDX] = CHAR_BASE(CHAR_BASE_IDX) | SCREEN_BASE(MAP_BASE_IDX) | BG_SIZE_3;
 
   // Load the tile image data.
-  CpuFastSet(white_allTiles, CHAR_BASE_ADR(CHAR_BASE_IDX), 512 | COPY32);
-  BG_PALETTE[0] = RGB5(0x0, 0x0, 0x0);
-  BG_PALETTE[1] = RGB5(0x0f, 0x0f, 0x0f);
+  CpuFastSet(piecesTiles, CHAR_BASE_ADR(CHAR_BASE_IDX), 512 | COPY32);
+  CpuFastSet(piecesPal, BG_PALETTE, 8 | COPY32);
 
   // Make sure scroll is at the top left.
   REG_BG1HOFS = 0;
---
+++ linkage/source/pieces.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(pieces)
+
+//======================================================================
+//
+//	pieces, 64x64@4, 
+//	+ palette 16 entries, not compressed
+//	+ 64 tiles not compressed
+//	Total size: 32 + 2048 = 2080
+//
+//	Time-stamp: 2021-10-18, 22:55:13
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_PIECES_H
+#define GRIT_PIECES_H
+
+#define piecesTilesLen 2048
+extern const unsigned int piecesTiles[512];
+
+#define piecesPalLen 32
+extern const unsigned short piecesPal[16];
+
+#endif // GRIT_PIECES_H
+
+//}}BLOCK(pieces)
